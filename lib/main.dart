1def649a711db8dd6674258b09348ba84787a1e7
Melissa Belfer <mrbelf5@gmail.com>
2019-02-02 11:40:48 -0500

    Testing

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
           title: Text('Welcome to Flutter'),
         ),
         body: Center(
-          child: Text('Hello World'),
+          child: Text('This is Hidden Gems'),
         ),
       ),
     );
