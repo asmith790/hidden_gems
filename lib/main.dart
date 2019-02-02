cdc509ba1ac66b4209c8171b84eda5a796399a6e
Melissa Belfer <mrbelf5@gmail.com>
2019-02-02 13:19:01 -0500

    FireBase Authentication Set up with Google Sign in

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() => runApp(MyApp());
 
