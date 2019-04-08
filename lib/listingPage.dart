3293349d3c42bebf777523f8c391be040532d67a
Althea Smith <asmith790@ufl.edu>
2019-04-08 17:30:13 -0400

    Search Implemented on list view

--- lib/listingPage.dart
+++ lib/listingPage.dart
@@ -1,34 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import 'navBar.dart';
 import 'post.dart';
+import 'postView.dart';
 
 class ListingPage extends StatefulWidget{
+  final List<Post> posts;
+
   @override
-  State<StatefulWidget> createState() => new ListingPageState();
+  ListingPage({this.posts});
+  State<StatefulWidget> createState() => new ListingPageState(posts);
 }
 
 class ListingPageState extends State<ListingPage> {
-  List<Post> posts = new List<Post>();
-  TextEditingController controller = new TextEditingController();
-  String filter;
+  List<Post> posts;
 
-//Place posts from Query into here
-  List<Post> getPosts(AsyncSnapshot<QuerySnapshot> snapshot) {
-    List<Post> queryPosts;
-    queryPosts = snapshot.data.documents
-        .map((document) =>
-    new Post(
-      description: document["description"],
-      name: document["name"],
-      tags: document["tags"].toString(),
-      rating: document["rating"],
-      userid: document["userid"],
-      imgUrl: document["picture"],
-    )).toList();
-    return queryPosts;
+  ListingPageState(List<Post> posts){
+    this.posts = posts;
   }
 
+  TextEditingController controller = new TextEditingController();
+  String filter = "";
+
+//Place posts from Query into here
   Image getPicture(String url) {
     if (url == "") {
       return Image.asset(
@@ -46,15 +38,15 @@ class ListingPageState extends State<ListingPage> {
   }
 
   @override
-//  initState() {
-//    super.initState();
-//    controller.addListener(() {
-//      setState(() {
-//        filter = controller.text;
-//      });
-//    });
-//    super.initState();
-//  }
+  initState() {
+    super.initState();
+    controller.addListener(() {
+      setState(() {
+        filter = controller.text;
+      });
+    });
+    super.initState();
+  }
 
     @override
     void dispose() {
@@ -64,106 +56,78 @@ class ListingPageState extends State<ListingPage> {
 
     @override
     Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(title: Text('Hidden Gems')),
-        body: StreamBuilder<QuerySnapshot>(
-          stream: Firestore.instance.collection('posts').where(
-              "finished", isEqualTo: true).snapshots(),
-          builder: (BuildContext context,
-              AsyncSnapshot<QuerySnapshot> snapshot) {
-            if (snapshot.hasError) return new Text('${snapshot.error}');
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-                return new TextField( //Work around
-                  onSubmitted: (text) {
-                    setState(() {
-                      filter = text;
-                    });
-                  },
-                  decoration: new InputDecoration(
-                      labelText: "Search"
-                  ),
-                  controller: controller,
-                );
-              default:
-                posts = getPosts(snapshot);
-                return new Column(
-                  children: <Widget>[
-                    TextField( //Work around
-                      onSubmitted: (text) {
-                        setState(() {
-                          filter = text;
-                        });
-                },
-                      decoration: new InputDecoration(
-                          labelText: "Search"
-                      ),
-                      controller: controller,
-                    ),
-                    ListView.builder(
-                        shrinkWrap: true,
-                        itemCount: posts.length,
-                        itemBuilder: (BuildContext context, int index) {
-                          return filter == null || filter == "" ?  new Text(posts[index].name) :
-                          posts[index].name.toLowerCase().contains(filter.toLowerCase()) ?  Text(posts[index].name) : new Container();
-                        }
-
-                    ),
-                  ],);
-            }
-          },
+      return Column(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
+          child: TextField( //Work around
+            decoration: new InputDecoration(
+                labelText: "Search",
+              suffixIcon: Icon(Icons.search),
+              enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue)
+              ),
+            ),
+            controller: controller,
+          ),
         ),
-        drawer: MyDrawer(),
-      );
+                      Expanded(
+                        child:
+
+                      ListView.builder(
+                          shrinkWrap: true,
+                          itemCount: posts.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return posts[index].name.toLowerCase().contains(filter.toLowerCase()) || posts[index].tags.toLowerCase().contains(filter.toLowerCase()) ? new Container (
+                                child: ListTile(
+                              title: new Text(
+                                posts[index].name,
+                                style: TextStyle(
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.blue,
+                                ),
+                              ),
+                              subtitle: new Row(children: <Widget>[
+                                new Text.rich(
+                                  TextSpan(
+                                    children: <TextSpan>[
+                                      TextSpan(
+                                          text: posts[index].description,
+                                          style: TextStyle(
+                                            fontSize: 18.0,
+                                          )),
+                                      TextSpan(
+                                          text: '\ndistance here\n',
+                                          style: TextStyle(
+                                              fontStyle: FontStyle.italic,
+                                              fontSize: 18.0)),
+                                      TextSpan(
+                                          text: posts[index].tags.toString(),
+                                          style: TextStyle(
+                                              fontSize: 18.0,
+                                              fontWeight: FontWeight.bold)),
+                                    ],
+                                  ),
+                                ),
+                              ]),
+                              isThreeLine: true,
+                              leading: Column(
+                                children: <Widget>[
+                                  getPicture(posts[index].imgUrl),
+                                ],
+                              ),
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(builder: (context) => new PostView(id: posts[index].id)),
+                                );
+                              },
+                            ),
+                            ): new Container();
+                          }
+
+                      ),
+      ),
+        ],);
     }
   }
-//
-//                children:
-//                    snapshot.data.documents.map((DocumentSnapshot document) {
-//                  return new ListTile(
-//                    title: new Text(
-//                      document['name'],
-//                      style: TextStyle(
-//                        fontSize: 22.0,
-//                        fontWeight: FontWeight.bold,
-//                        color: Colors.blue,
-//                      ),
-//                    ),
-//                    subtitle: new Row(children: <Widget>[
-//                      new Text.rich(
-//                        TextSpan(
-//                          children: <TextSpan>[
-//                            TextSpan(
-//                                text: document['description'],
-//                                style: TextStyle(
-//                                  fontSize: 18.0,
-//                                )),
-//                            TextSpan(
-//                                text: '\ndistance here',
-//                                style: TextStyle(
-//                                    fontStyle: FontStyle.italic,
-//                                    fontSize: 18.0)),
-//                            TextSpan(
-//                                text: '\n${document['tags'].toString()}',
-//                                style: TextStyle(
-//                                    fontSize: 18.0,
-//                                fontWeight: FontWeight.bold)),
-//                          ],
-//                        ),
-//                      ),
-//                    ]),
-//                    isThreeLine: true,
-//                    leading: Column(
-//                      children: <Widget>[
-//                      getPicture(document['picture']),
-//                      ],
-//                    ),
-//                    onTap: () {
-//                      print('document id: ' + document.documentID);
-//                      Navigator.push(
-//                        context,
-//                        new MaterialPageRoute(builder: (context) => new PostView(id: document.documentID)),
-//                      );
-//                    },
-//                  );
-//                }).toList(),
