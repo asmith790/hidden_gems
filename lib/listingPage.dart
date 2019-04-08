a9c7cf28367200931aee544c2c0e089b56aae9ee
Althea Smith <asmith790@ufl.edu>
2019-04-08 12:09:31 -0400

    checkpoint

--- lib/listingPage.dart
+++ lib/listingPage.dart
@@ -1,13 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'navBar.dart';
-import 'postView.dart';
-import 'newPost.dart';
+import 'post.dart';
 
-class ListingPage extends StatelessWidget {
+class ListingPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => new ListingPageState();
+}
 
-  Image getPicture(String url){
-    if(url == ""){
+class ListingPageState extends State<ListingPage> {
+  List<Post> posts = new List<Post>();
+  TextEditingController controller = new TextEditingController();
+  String filter;
+
+//Place posts from Query into here
+  List<Post> getPosts(AsyncSnapshot<QuerySnapshot> snapshot) {
+    List<Post> queryPosts;
+    queryPosts = snapshot.data.documents
+        .map((document) =>
+    new Post(
+      description: document["description"],
+      name: document["name"],
+      tags: document["tags"].toString(),
+      rating: document["rating"],
+      userid: document["userid"],
+      imgUrl: document["picture"],
+    )).toList();
+    return queryPosts;
+  }
+
+  Image getPicture(String url) {
+    if (url == "") {
       return Image.asset(
         //Would become a photo
         'assets/gem.png',
@@ -21,94 +44,126 @@ class ListingPage extends StatelessWidget {
       width: 76.0,
     );
   }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Hidden Gems')),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: Firestore.instance.collection('posts').where("finished", isEqualTo: true).snapshots(),
-        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-          if (snapshot.hasError) return new Text('${snapshot.error}');
-          switch (snapshot.connectionState) {
-            case ConnectionState.waiting:
-              return new Center(child: new CircularProgressIndicator());
-            default:
-              return new ListView(  //Might have to use a listview builder and specify item count?
-                //itemCount: snapshot.data.documents.length,
-                children:
-                    snapshot.data.documents.map((DocumentSnapshot document) {
-                  return new ListTile(
-                    title: new Text(
-                      document['name'],
-                      style: TextStyle(
-                        fontSize: 22.0,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.blue,
-                      ),
-                    ),
-                    subtitle: new Row(children: <Widget>[
-                      new Text.rich(
-                        TextSpan(
-                          children: <TextSpan>[
-                            TextSpan(
-                                text: document['description'],
-                                style: TextStyle(
-                                  fontSize: 18.0,
-                                )),
-                            TextSpan(
-                                text: '\ndistance here',
-                                style: TextStyle(
-                                    fontStyle: FontStyle.italic,
-                                    fontSize: 18.0)),
-                            TextSpan(
-                                text: '\n${document['tags'].toString()}',
-                                style: TextStyle(
-                                    fontSize: 18.0,
-                                fontWeight: FontWeight.bold)),
-                          ],
-                        ),
-                      ),
-                    ]),
-                    isThreeLine: true,
-                    leading: Column(
-                      children: <Widget>[
-                      getPicture(document['picture']),
-                      ],
-                    ),
-                    onTap: () {
-                      print('document id: ' + document.documentID);
-                      Navigator.push(
-                        context,
-                        new MaterialPageRoute(builder: (context) => new PostView(id: document.documentID)),
-                      );
-                    },
-                  );
-                }).toList(),
-              );
-          }
-        },
-      ),
-      drawer: MyDrawer(),
-      floatingActionButton: FloatingActionButton.extended(
-          onPressed: () { Navigator.push(
-      context,
-      new MaterialPageRoute(builder: (context) => new CustomForm()),
-      );
-      },
-          icon: Icon(Icons.add),
-          label: Text("New Gem")),
-//      persistentFooterButtons: <Widget>[
-//        ButtonBar(
-//          mainAxisSize: MainAxisSize.max,
-//          alignment: MainAxisAlignment.center,
-//          children: <Widget>[
-//            RaisedButton(
-//              child: new Text("New Gem"),
-//            )
-//          ],
-//        ),
-//      ],
-    );
-  }
-}
 
+  @override
+//  initState() {
+//    super.initState();
+//    controller.addListener(() {
+//      setState(() {
+//        filter = controller.text;
+//      });
+//    });
+//    super.initState();
+//  }
+
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(title: Text('Hidden Gems')),
+        body: StreamBuilder<QuerySnapshot>(
+          stream: Firestore.instance.collection('posts').where(
+              "finished", isEqualTo: true).snapshots(),
+          builder: (BuildContext context,
+              AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) return new Text('${snapshot.error}');
+            switch (snapshot.connectionState) {
+              case ConnectionState.waiting:
+                return new TextField( //Work around
+                  onSubmitted: (text) {
+                    setState(() {
+                      filter = text;
+                    });
+                  },
+                  decoration: new InputDecoration(
+                      labelText: "Search"
+                  ),
+                  controller: controller,
+                );
+              default:
+                posts = getPosts(snapshot);
+                return new Column(
+                  children: <Widget>[
+                    TextField( //Work around
+                      onSubmitted: (text) {
+                        setState(() {
+                          filter = text;
+                        });
+                },
+                      decoration: new InputDecoration(
+                          labelText: "Search"
+                      ),
+                      controller: controller,
+                    ),
+                    ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: posts.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return filter == null || filter == "" ?  new Text(posts[index].name) :
+                          posts[index].name.toLowerCase().contains(filter.toLowerCase()) ?  Text(posts[index].name) : new Container();
+                        }
+
+                    ),
+                  ],);
+            }
+          },
+        ),
+        drawer: MyDrawer(),
+      );
+    }
+  }
+//
+//                children:
+//                    snapshot.data.documents.map((DocumentSnapshot document) {
+//                  return new ListTile(
+//                    title: new Text(
+//                      document['name'],
+//                      style: TextStyle(
+//                        fontSize: 22.0,
+//                        fontWeight: FontWeight.bold,
+//                        color: Colors.blue,
+//                      ),
+//                    ),
+//                    subtitle: new Row(children: <Widget>[
+//                      new Text.rich(
+//                        TextSpan(
+//                          children: <TextSpan>[
+//                            TextSpan(
+//                                text: document['description'],
+//                                style: TextStyle(
+//                                  fontSize: 18.0,
+//                                )),
+//                            TextSpan(
+//                                text: '\ndistance here',
+//                                style: TextStyle(
+//                                    fontStyle: FontStyle.italic,
+//                                    fontSize: 18.0)),
+//                            TextSpan(
+//                                text: '\n${document['tags'].toString()}',
+//                                style: TextStyle(
+//                                    fontSize: 18.0,
+//                                fontWeight: FontWeight.bold)),
+//                          ],
+//                        ),
+//                      ),
+//                    ]),
+//                    isThreeLine: true,
+//                    leading: Column(
+//                      children: <Widget>[
+//                      getPicture(document['picture']),
+//                      ],
+//                    ),
+//                    onTap: () {
+//                      print('document id: ' + document.documentID);
+//                      Navigator.push(
+//                        context,
+//                        new MaterialPageRoute(builder: (context) => new PostView(id: document.documentID)),
+//                      );
+//                    },
+//                  );
+//                }).toList(),
