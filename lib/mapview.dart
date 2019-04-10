221d792cbbdd132e75517c55156b587e76888270
Althea Smith <asmith790@ufl.edu>
2019-04-09 22:08:09 -0400

    Markers show up on map

--- lib/mapview.dart
+++ lib/mapview.dart
@@ -11,27 +11,31 @@ class MapV extends State<MapsDemo> {
   GoogleMapController mapController;
   static const LatLng _center = const LatLng(29.6516, -82.3248);
 
-  List <LatLng> allMarkers;
+ // List <GeoPoint> allMarkers;
 
   void _updateMarkers() {
       Firestore.instance.collection('posts')
           .where("finished", isEqualTo: true)
           .snapshots()
           .listen((data) =>
-          data.documents.forEach((doc) => print(doc["position"])));
-          //data.documents.forEach((doc) => allMarkers.add(doc["position"])));
+          //data.documents.forEach((doc) => print(doc["position"])));
+          //data.documents.forEach((doc) => allMarkers.add(doc["position"]))
+      data.documents.forEach((doc) => mapController.addMarker(MarkerOptions(
+        position: LatLng(doc["position"].latitude, doc["position"].longitude),
+      )))
+      );
           //print(allMarkers);
   }
 
-  void marker() {
-    mapController.addMarker(
-      MarkerOptions(
-        //icon: BitmapDescriptor.fromAsset("assets/gemicon.bmp"),
-        //position: LatLng(29.6516, -82.3248),
-        position: LatLng(allMarkers[0].latitude, allMarkers[0].longitude),
-      ),
-    );
-  }
+//  void marker() {
+//    mapController.addMarker(
+//      MarkerOptions(
+//        //icon: BitmapDescriptor.fromAsset("assets/gemicon.bmp"),
+//        //position: LatLng(29.6516, -82.3248),
+//        position: LatLng(allMarkers[0].latitude, allMarkers[0].longitude),
+//      ),
+//    );
+//  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,7 +49,7 @@ class MapV extends State<MapsDemo> {
           onMapCreated: (GoogleMapController controller) {
             mapController = controller;
             _updateMarkers();
-            marker();
+            //marker();
           },
           options: GoogleMapOptions(
             scrollGesturesEnabled: true,
